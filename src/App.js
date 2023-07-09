8e8209e157c19ecb8c7e817313409c14403bb6d4
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2023-07-09 16:29:40 +0300

    Added Edit component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Catalog } from './components/Catalog/Catalog';
 import { Create } from './components/Create/Create';
 import { Details } from './components/Details/Details';
+import { Edit } from './components/Edit/Edit';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
@@ -17,31 +18,7 @@ function App() {
     <Login/>
     <Register/>
     <Create/>
-  {/* Edit Page ( Only for the creator )*/}
-  <section id="edit-page" className="auth">
-    <form id="edit">
-      <div className="container">
-        <h1>Edit Game</h1>
-        <label htmlFor="leg-title">Legendary title:</label>
-        <input type="text" id="title" name="title" defaultValue="" />
-        <label htmlFor="category">Category:</label>
-        <input type="text" id="category" name="category" defaultValue="" />
-        <label htmlFor="levels">MaxLevel:</label>
-        <input
-          type="number"
-          id="maxLevel"
-          name="maxLevel"
-          min={1}
-          defaultValue=""
-        />
-        <label htmlFor="game-img">Image:</label>
-        <input type="text" id="imageUrl" name="imageUrl" defaultValue="" />
-        <label htmlFor="summary">Summary:</label>
-        <textarea name="summary" id="summary" defaultValue={""} />
-        <input className="btn submit" type="submit" defaultValue="Edit Game" />
-      </div>
-    </form>
-  </section>
+    <Edit/>
     <Details/>
     <Catalog/>
 </div>
